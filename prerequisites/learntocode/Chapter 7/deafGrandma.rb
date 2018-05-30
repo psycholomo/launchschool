d52eb424b2c948471f469ecf132869a7678cb7d3
josh Crane <psycholomo@gmail.com>
2018-05-30 00:52:14 -0700

    refactoring the deaf grandma exercise to use the extended version with random years

---
+++ prerequisites/learntocode/Chapter 7/deafGrandma.rb
@@ -0,0 +1,32 @@
+
+counter = 0
+puts "say something to grandma, exit by saying BYE 3 times"
+while true
+  
+  question = gets.chomp
+
+    
+
+  
+
+  if question == "BYE"
+    counter +=1
+     puts "HUH?! SPEAK UP, SONNY!"
+    
+
+  else
+  puts "HUH?! SPEAK UP, SONNY!"
+  counter = 0 
+    
+
+  end
+
+   break if counter == 3
+ end
+
+ 
+
+end
+
+ random_year = rand(1930..1950)
+  puts "NO, NOT SINCE #{random_year}!"  
---
+++ prerequisites/learntocode/Chapter 7/leapYears.rb
@@ -0,0 +1,6 @@
+puts "We want to find all the leap years between a given date range."
+puts "Please enter a start date: "
+start_date = 1800
+puts "Please enter an end date:"
+end_date = 2004
+
