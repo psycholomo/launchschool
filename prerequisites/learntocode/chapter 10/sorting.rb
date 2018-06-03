4a8d59e1e3709b6a8af6a86335ea8bd4e0216656
josh Crane <psycholomo@gmail.com>
2018-06-02 22:49:40 -0700

    Completed the manual sorting algorithm in chapter 10 'learn to code'

--- prerequisites/learntocode/chapter 10/sorting.rb
+++ prerequisites/learntocode/chapter 10/sorting.rb
@@ -1,1 +1,45 @@
-# sort an array of words alphabetically, ignore upper case and lower case
+# sort an array of words alphabetically manually, ignore upper case and lower case, remove duplicates
+
+array_of_words = ["this", "apple","is","really","yummy","to","eat", "yummy", "Yummy"]
+
+
+
+
+def sorter_try(array=[])
+
+downcase_words(array)
+sorted_words_array = []
+
+while array.length > 0
+ i = 0 
+ placeholder_word = "".downcase
+
+for words in array
+  
+  i = i + 1 
+
+  if array[i - 1] > placeholder_word
+    placeholder_word = words
+    
+
+  end
+
+end
+
+  sorted_words_array.push(placeholder_word)
+  array.delete(placeholder_word)
+
+end
+
+  return p sorted_words_array.reverse
+end
+
+
+def downcase_words(array)
+  array.map(&:downcase!)
+
+end
+
+sorter_try(array_of_words)
+
+
