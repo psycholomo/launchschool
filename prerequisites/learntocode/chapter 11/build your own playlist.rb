160cf629f46e3c9a24bc929b9d18a18146f32b18
josh Crane <psycholomo@gmail.com>
2018-06-06 13:48:00 -0700

    finished the safter picture downloading and completed all exercises for the build your own playlist

--- prerequisites/learntocode/chapter 11/build your own playlist.rb
+++ prerequisites/learntocode/chapter 11/build your own playlist.rb
@@ -1,2 +1,43 @@
 #create a playlist that will save as a .m3u extension. It can be in a text file 
 # or an array but make sure its truly random regardless of the folder
+
+
+Dir.chdir '/Users/onji/Music/BTS - The Most Beautiful Moment in Life Young Forever [Special Album]/'
+
+#music_list = Dir.glob('/users/onji/Music/BTS\ -\ The\ Most\ Beautiful\ Moment\ in\ Life\ Young\ Forever\ \[Special\ Album\]/**/*.mp3').select {|f| File.file?(f)}
+music_list = Dir.entries("./[CD 2]")
+music_list2 = Dir.entries("./[CD 1]")
+test_file = File.open("test.m3u", "w+")
+$music_array = []
+
+
+
+def music_push(array)
+
+  array.each do |music|
+    if(music == "." || music == ".." || music.slice(-3..-1) != "mp3")
+    next
+  else
+  $music_array.push(music)
+  end
+end
+end
+
+
+
+music_push(music_list)
+music_push(music_list2)
+
+
+$music_array.shuffle
+
+test_file.write($music_array)
+
+test_file.close
+
+puts $music_array 
+
+
+
+
+
--- prerequisites/learntocode/chapter 11/safer-picture-downloading.rb
+++ prerequisites/learntocode/chapter 11/safer-picture-downloading.rb
@@ -1,3 +1,30 @@
 #take a folder of pictures, Download i.e. copy into a different folder and check if 
 #the files currently exist.
 
+
+Dir.chdir '/Users/onji/Desktop/new Stuff'
+target_directory = 'Users/onji/Desktop/new Stuff'
+files_to_copy = Dir.glob('/Users/onji/Desktop/old Stuff/**/*.png')
+picture_number = 1
+batch_name = 'work_screenshots'
+puts "Downloading #{files_to_copy.length} files in the old directory"
+
+files_to_copy.each do |name|
+  print '.'
+  new_name = if picture_number < 10
+    "#{batch_name}0#{picture_number}.png"
+  else 
+    "#{batch_name}#{picture_number}.png"
+  end
+
+  if File.exist(new_name) == true
+    break
+  else
+  File.rename name, new_name
+  picture_number += 1
+end
+end
+
+puts #
+
+puts "Copy and rename is complete"
