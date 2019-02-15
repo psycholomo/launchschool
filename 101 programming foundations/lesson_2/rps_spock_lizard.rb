2f2ef561c62e9a30444480ee0835220870411a64
Josh <psycholomo@gmail.com>
2019-02-14 17:10:21 -0800

    finished rps_spock_lizard game and got rubocop down to 1 error

--- 101 programming foundations/lesson_2/rps_spock_lizard.rb
+++ 101 programming foundations/lesson_2/rps_spock_lizard.rb
@@ -1,54 +1,83 @@
-VALID_CHOICES = {"rock" => ["lizard", "scissors"],
-                "lizard" => ["paper", "spock"],
-                "spock" => ["scissors", "rock"],
-                "scissors" => ["paper", "lizard"],
-                "paper" => ["rock", "spock"]
-                }
+VALID_CHOICES = { "rock" => ["lizard", "scissors"],
+                  "lizard" => ["paper", "spock"],
+                  "spock" => ["scissors", "rock"],
+                  "scissors" => ["paper", "lizard"],
+                  "paper" => ["rock", "spock"] }
 
-scoreboard = [0, 0]
+COMPUTER_INPUTS = ['rock', 'paper', 'scissors', "spock", "lizard"]
+
+USER_INPUTS = { 'r' => 'rock',
+                'p' => 'paper',
+                'sc' => 'scissors',
+                'sp' => 'spock',
+                'l' => 'lizard' }
+
+@scoreboard = [0, 0]
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def score_tally()
-  
+def print_score
+  print("=> the score is now #{@scoreboard} \n")
 end
 
-def results(user_result, computer_result)
-  if VALID_CHOICES[user_result][0] == computer_result || VALID_CHOICES[user_result][1] == computer_result
-    puts "you win"
-    return true
-  elsif user_result == VALID_CHOICES[computer_result][0] || user_result == VALID_CHOICES[computer_result][1]
-    puts "you lose"
-    return false
-  else 
-    puts "its a tie"
-    return false
+def score_tally(result)
+  if result == 0
+    @scoreboard[0] += 1
+  elsif result == 1
+    @scoreboard[1] += 1
   end
+end
+
+def results(user_result, comp_result)
+  if VALID_CHOICES[user_result][0] == comp_result ||
+     VALID_CHOICES[user_result][1] == comp_result
+    score_tally(0)
+    prompt("you win")
+
+  elsif user_result == VALID_CHOICES[comp_result][0] ||
+        user_result == VALID_CHOICES[comp_result][1]
+    score_tally(1)
+    prompt("you lose")
+  else
+    prompt("its a tie")
+  end
+  print_score
 end
 
 loop do
   choice = ''
 
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("Choose one: #{COMPUTER_INPUTS.join(', ')}")
+    prompt("you can also use the short hand of 'r' for rock, 'p' for paper,")
+    prompt("'sc' for scissors, 'sp' for spock, and 'l' for lizard")
     choice = Kernel.gets().chomp
-    if VALID_CHOICES.include?(choice)
+    if USER_INPUTS.key?(choice)
+      choice = USER_INPUTS[choice]
+      break
+    elsif USER_INPUTS.value?(choice)
       break
     else
       prompt("That's not a valid choice")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = COMPUTER_INPUTS.sample
 
   Kernel.puts("you chose #{choice}, the computer chose #{computer_choice}.")
 
-  display_results(choice, computer_choice)
+  results(choice, computer_choice)
 
-  prompt("Do you want to play again?")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  if @scoreboard[0] == 5
+    prompt("you won the grand tournament!")
+    break
+  elsif @scoreboard[1] == 5
+    prompt("you lost the grand tournament.")
+    break
+  end
 end
+
 prompt("Thanks for playing. Goodbye!")
+print @scoreboard
