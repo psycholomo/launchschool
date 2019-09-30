5ef986422fffeaff58ac29640ef275b8f1dfed2a
psycholomo <psycholomo@gmail.com>
2019-09-29 21:43:54 -0700

    sync between machines

--- 120 OOP/lesson_5/ttt-oo.rb
+++ 120 OOP/lesson_5/ttt-oo.rb
@@ -1,10 +1,24 @@
 class Board
+
+  INITIAL_MARKER = " "
   def initialize
+    @squares = {}
+    (1..9).each {|key| @squares[key] = Square.new(INITIAL_MARKER)}
+
+  end
+
+  def get_square_at(num)
+    @squares[num]
   end
 end
 
 class Square
-  def initialize
+  def initialize(marker)
+    @marker = marker
+  end
+
+  def to_s
+    @marker
   end
 end
 
@@ -17,6 +31,38 @@ class Player
 end
 
 class TTTGame
+  attr_reader :board
+  def initialize
+    
+    @board = Board.new
+
+  end
+
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+    puts ""
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+  end
+
+  def display_board
+  puts "+------+------+-----+"
+  puts "|      |      |     |"
+  puts "|   #{board.get_square_at(1)}  |   #{board.get_square_at(2)}  |  #{board.get_square_at(3)}  |"
+  puts "|      |      |     |"
+  puts "+------+------+-----+"
+  puts "|      |      |     |"
+  puts "|   #{board.get_square_at(4)}  |   #{board.get_square_at(5)}  |  #{board.get_square_at(6)}  |"
+  puts "|      |      |     |"
+  puts "+------+------+-----+"
+  puts "|      |      |     |"
+  puts "|   #{board.get_square_at(7)}  |   #{board.get_square_at(8)}  |  #{board.get_square_at(9)}  |"
+  puts "|      |      |     |"
+  puts "+------+------+-----+"
+  end
+
   def play
     display_welcome_message
     loop do
