f2c30706bf39cabc77568acf299a94753163dbc2
psycholomo <psycholomo@gmail.com>
2020-03-21 00:23:35 -0700

    more stuff

--- 175-networkapps/server.rb
+++ 175-networkapps/server.rb
@@ -1,19 +1,50 @@
 require "socket"
 
-server = TCPServer.new("localhost", 3001)
+def parse_request(request_line)
+    http_method, path_and_params, http = request_line.split(" ")
+
+  path, params = path_and_params.split("?")
+
+   params = (params || "").split("&").each_with_object({}) do |pair, hash|
+     key, value = pair.split("=")
+     hash[key] = value
+   end
+   [http_method, path, params]
+end
+
+
+server = TCPServer.new("localhost", 3005)
 
 loop do 
   client = server.accept
 
-  request_line = client.gets
-  next if !request_line || request_line =~ /favicon/
-  puts request_line
+   request_line = client.gets
+     next if !request_line || request_line =~ /favicon/
+   puts request_line
 
-  "GET /?rolls=2&sides=6 HTTP/1.1"
-  client.puts "HTTP/1.1 200 OK\r\n\r\n"
-  client.puts request_line
-  client.puts "Content-Type: text/plain\r\n\r\n"
-  client.puts rand(6) + 1
+
+  http_method, path, params = parse_request(request_line)
+
+
+
+  client.puts "HTTP/1.0 200 OK"
+  client.puts "Content-Type: text/html"
+  client.puts
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
+    client.puts path
+  client.puts params
+  client.puts http_method
+  client.puts "</pre>"
+  client.puts "<h1>Counter</h1>"
+  number = params["number"].to_i
+  client.puts "<p>The current number is #{number}.</p>"
+
+  client.puts "<a href='?number=#{number + 1}'> Add one</a>"
+    client.puts "<a href='?number=#{number - 1}'> Subtract one</a>"
+  client.puts "</body>"
+  client.puts "</html>"
   client.close
   
 end
